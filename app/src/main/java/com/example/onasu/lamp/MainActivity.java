27aca1a471fa7e54acdbaeff87b63b0355c3411d
Benzaza <benzzamakon2@gmail.com>
2017-02-01 20:48:21 +0700

    GraphView , Line Point

--- app/src/main/java/com/example/onasu/lamp/MainActivity.java
+++ app/src/main/java/com/example/onasu/lamp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.onasu.lamp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.DataInput;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //** GraphView
+        GraphView graphView = (GraphView) findViewById(R.id.graphView);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                //** Line
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+
+        graphView.addSeries(series);
     }
 }
