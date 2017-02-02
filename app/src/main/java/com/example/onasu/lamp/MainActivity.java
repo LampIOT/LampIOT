0c6d572090fe22e09357bee433675a8bdd6b212f
Benzaza <benzzamakon2@gmail.com>
2017-02-02 14:25:32 +0700

    Announce x,y and upauto v.1.0.0

--- app/src/main/java/com/example/onasu/lamp/MainActivity.java
+++ app/src/main/java/com/example/onasu/lamp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.onasu.lamp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -10,12 +11,21 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import java.io.DataInput;
 
 public class MainActivity extends AppCompatActivity {
-
+  //  private FirebaseAnalytics mFirebaseAnalytics;
+  //  mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //** Announce x and y
+      //  double x = 0 , y = 0;
+
+        //** sum x
+
+        //x = x + 1;
+
         //** GraphView
+
         GraphView graphView = (GraphView) findViewById(R.id.graphView);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
                 //** Line
@@ -23,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(4, 6)//,
+               // new DataPoint(x,y)
         });
 
         graphView.addSeries(series);
