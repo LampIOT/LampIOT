3d4501a63519a68470083d68e9b4738fbe7b7954
id10bat <my_nutthapat-26_@hotmail.com>
2017-02-03 14:41:03 -0800

    ทำการเชื่อมต่อปุ่มกับ FirebaseDatabase

--- app/src/main/java/com/example/onasu/lamp/MainActivity.java
+++ app/src/main/java/com/example/onasu/lamp/MainActivity.java
@@ -2,21 +2,57 @@ package com.example.onasu.lamp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.io.DataInput;
+
+
 
 public class MainActivity extends AppCompatActivity {
-  //  private FirebaseAnalytics mFirebaseAnalytics;
+    private ToggleButton mToggle;
+    private TextView mText;
+
+    //กำหนดค่าให้เชื่อมต่อกับ FirebaseDatabase
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Lamp");
+
+
+
+
+    //  private FirebaseAnalytics mFirebaseAnalytics;
   //  mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ////ปุ่มกด off-on
+        mToggle=(ToggleButton)findViewById(R.id.toggleButton);
+        mText=(TextView)findViewById(R.id.textView2);
+
+
+        //คลิกเพื่อส่งข้อมูลไปยัง FirebaseDatabase
+        mToggle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                if (mToggle.isChecked()){
+                    myRef.setValue("0");
+                    mText.setText("On");
+                }
+                else
+                {
+                    myRef.setValue("1");
+                    mText.setText("Off");
+                }
+            }
+        });
         //** Announce x and y
       //  double x = 0 , y = 0;
 
